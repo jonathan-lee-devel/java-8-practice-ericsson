820bd3162660b58679435e37588c70a6334b01bb
Jonathan Lee <jonathan.lee.devel@gmail.com>
2021-11-04 04:12:53 +0000

    Tidied code

--- src/main/java/io/jonathanlee/App.java
+++ src/main/java/io/jonathanlee/App.java
@@ -1,7 +1,5 @@
 package io.jonathanlee;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -14,40 +12,43 @@ public class App {
 
     public static void main( String[] args ) throws InterruptedException {
 
-        Stream<String> generatedSequentialStream = Stream.generate(() -> UUID.randomUUID().toString()).limit(Long.MAX_VALUE).sequential();
-        if (!generatedSequentialStream.isParallel()) {
-            long start = System.currentTimeMillis();
-            boolean containsLarge = generatedSequentialStream
-                    .map(String::length)
-                    .anyMatch(length -> length > 5);
-            long elapsed = System.currentTimeMillis() - start;
-
-            System.out.println("Sequential contains large: " + containsLarge);
-            System.out.println("Time elapsed: " + elapsed + "ms!");
-        }
+        Stream<String> generatedSequentialStream = generateStream(false);
+        performBenchmark(generatedSequentialStream);
         generatedSequentialStream.close();
 
         Thread.sleep(2000L);
 
-        Stream<String> generatedParallelStream = Stream.generate(() -> UUID.randomUUID().toString()).limit(Long.MAX_VALUE).parallel();
-        if (generatedParallelStream.isParallel()) {
-            long start = System.currentTimeMillis();
-            boolean containsLarge = generatedParallelStream
-                    .map(String::length)
-                    .anyMatch(length -> length > 5);
-            long elapsed = System.currentTimeMillis() - start;
-
-            System.out.println("Parallel contains large: " + containsLarge);
-            System.out.println("Time elapsed: " + elapsed + "ms!");
-        }
+        Stream<String> generatedParallelStream = generateStream(true);
+        performBenchmark(generatedParallelStream);
         generatedSequentialStream.close();
     }
 
-    public static Stream<String> streamOf(List<String> list) {
+    private static Stream<String> generateStream(boolean isParallel) {
+        Stream<String> generatedStream = Stream
+                .generate(() -> UUID.randomUUID().toString())
+                .limit(Long.MAX_VALUE);
+
+        return isParallel ? generatedStream.parallel() : generatedStream.sequential();
+    }
+
+    private static void performBenchmark(Stream<String> stream) {
+        long start = System.currentTimeMillis();
+        boolean containsLarge = stream
+                .map(String::length)
+                .anyMatch(length -> length > 5);
+        long elapsed = System.currentTimeMillis() - start;
+
+        System.out.println("-----------------------------------");
+        System.out.println("Is parallel: " + stream.isParallel());
+        System.out.println("Contains large: " + containsLarge);
+        System.out.println("Time elapsed: " + elapsed + "ms!");
+    }
+
+    private static Stream<String> streamOf(List<String> list) {
         return list == null || list.isEmpty() ? Stream.empty() : list.stream();
     }
 
-    public static Stream<String> parallelStreamOf(List<String> list) {
+    private static Stream<String> parallelStreamOf(List<String> list) {
         return list == null || list.isEmpty() ? Stream.empty() : list.parallelStream();
     }
 
