599e38c7e5a6a4808453e124fdd53093d1cb4c5f
Jonathan Lee <jonathan.lee.devel@gmail.com>
2021-11-12 18:53:33 +0000

    Minor changes

--- src/main/java/io/jonathanlee/App.java
+++ src/main/java/io/jonathanlee/App.java
@@ -4,52 +4,50 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-/**
- * Hello world!
- *
- */
 public class App {
 
-    public static void main( String[] args ) throws InterruptedException {
+  public static void main(String[] args) throws InterruptedException {
+    Stream<String> generatedParallelStream = generateStream(true);
+    performBenchmark(generatedParallelStream);
+    generatedParallelStream.close();
 
-        Stream<String> generatedSequentialStream = generateStream(false);
-        performBenchmark(generatedSequentialStream);
-        generatedSequentialStream.close();
+    Thread.sleep(2000L);
 
-        Thread.sleep(2000L);
+    Stream<String> generatedSequentialStream = generateStream(false);
+    performBenchmark(generatedSequentialStream);
+    generatedSequentialStream.close();
+  }
 
-        Stream<String> generatedParallelStream = generateStream(true);
-        performBenchmark(generatedParallelStream);
-        generatedSequentialStream.close();
-    }
+  private static Stream<String> generateStream(boolean isParallel) {
+    Stream<String> generatedStream = Stream
+        .generate(() -> UUID.randomUUID().toString())
+        .limit(100_000_000L);
 
-    private static Stream<String> generateStream(boolean isParallel) {
-        Stream<String> generatedStream = Stream
-                .generate(() -> UUID.randomUUID().toString())
-                .limit(Long.MAX_VALUE);
+    return isParallel ? generatedStream.parallel() : generatedStream.sequential();
+  }
 
-        return isParallel ? generatedStream.parallel() : generatedStream.sequential();
-    }
+  private static void performBenchmark(Stream<String> stream) {
+    System.out.println("-----------------------------------");
+    System.out.println("Benchmarking...");
+    System.out.println("Is parallel: " + stream.isParallel());
 
-    private static void performBenchmark(Stream<String> stream) {
-        long start = System.currentTimeMillis();
-        boolean containsLarge = stream
-                .map(String::length)
-                .anyMatch(length -> length > 5);
-        long elapsed = System.currentTimeMillis() - start;
+    long start = System.currentTimeMillis();
+    boolean containsSmall = stream
+        .map(String::length)
+        .anyMatch(length -> length < 5);
+    long elapsed = System.currentTimeMillis() - start;
 
-        System.out.println("-----------------------------------");
-        System.out.println("Is parallel: " + stream.isParallel());
-        System.out.println("Contains large: " + containsLarge);
-        System.out.println("Time elapsed: " + elapsed + "ms!");
-    }
+    System.out.println("Contains small: " + containsSmall);
+    System.out.println("Time elapsed: " + elapsed + "ms!");
+    System.out.println("-----------------------------------");
+  }
 
-    private static Stream<String> streamOf(List<String> list) {
-        return list == null || list.isEmpty() ? Stream.empty() : list.stream();
-    }
+  private static Stream<String> streamOf(List<String> list) {
+    return list == null || list.isEmpty() ? Stream.empty() : list.stream();
+  }
 
-    private static Stream<String> parallelStreamOf(List<String> list) {
-        return list == null || list.isEmpty() ? Stream.empty() : list.parallelStream();
-    }
+  private static Stream<String> parallelStreamOf(List<String> list) {
+    return list == null || list.isEmpty() ? Stream.empty() : list.parallelStream();
+  }
 
 }
